982fa4e8a57bc782fa2a99da76a08be0f7c22597
imask333 <114331111+imask333@users.noreply.github.com>
2022-10-13 11:20:17 -0700

    new

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,17 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+  @Test
+  public void testReverseInPlace2(){
+    int[] input2 = {1, 2, 3};
+    //3, 2, 3
+    //3, 2, 3, 
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{3, 2, 1}, input2);
+  }
+  @Test 
+  public void testReverse2(){
+    int[] input3 = { 4,5,6};
+    assertArrayEquals(new int[]{6, 5, 4}, ArrayExamples.reversed(input3));
+  }
 }
